7168d059e7757fc6c44967883c12500aff19d5f8
Meet Gandhi <meetgandhi4041@gmail.com>
2025-11-05 00:47:44 +0530

    index.js

--- Althub-Server/index.js
+++ Althub-Server/index.js
@@ -1,38 +1,36 @@
 require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const connectToMongo = require("../db/conn");
-
-// Connect MongoDB
-connectToMongo();
-
 const app = express();
+const mongoose = require("mongoose");
+const connectToMongo = require("./db/conn");
+const cookieParser = require("cookie-parser");
+const port = 5001;
+const cors = require("cors");
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-// Middleware
+connectToMongo();
+//routes
+const user_route = require("./routes/userRoute");
+const event_route = require("./routes/eventRoute");
+const institute_route = require("./routes/instituteRoute");
+const course_route = require("./routes/courseRoute");
+const feedback_route = require("./routes/feedbackRoute");
+const post_route = require("./routes/postRoute");
+const admin_route = require("./routes/adminRoute");
+const conversation_route = require("./routes/conversationRoute");
+const message_route = require("./routes/messageRoute");
+const education_route = require("./routes/educationRoute");
+const experience_route = require("./routes/experienceRoute");
+const company_route = require("./routes/companyRoute");
+const notification_route = require("./routes/notificationRoute");
+const financialaid_route = require("./routes/financialaidRoute");
+
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.static("public"));
-
-// Import routes
-const user_route = require("../routes/userRoute");
-const event_route = require("../routes/eventRoute");
-const institute_route = require("../routes/instituteRoute");
-const course_route = require("../routes/courseRoute");
-const feedback_route = require("../routes/feedbackRoute");
-const post_route = require("../routes/postRoute");
-const admin_route = require("../routes/adminRoute");
-const conversation_route = require("../routes/conversationRoute");
-const message_route = require("../routes/messageRoute");
-const education_route = require("../routes/educationRoute");
-const experience_route = require("../routes/experienceRoute");
-const company_route = require("../routes/companyRoute");
-const notification_route = require("../routes/notificationRoute");
-const financialaid_route = require("../routes/financialaidRoute");
-
-// Use routes
 app.use("/api", user_route);
 app.use("/api", event_route);
 app.use("/api", institute_route);
@@ -48,68 +46,69 @@ app.use("/api", company_route);
 app.use("/api", notification_route);
 app.use("/api", financialaid_route);
 
-// Root endpoint
 app.get("/", (req, res) => {
-  res.status(200).send("Server is running on Vercel ✅");
+  res.end("Hellooo");
 });
 
-// Export app (Vercel will handle listening)
-module.exports = app;
+app.use(express.static("public"));
 
+app.listen(port, function () {
+  console.log("Server is ready");
+});
 
-// //socket server--------------------
-// let users = [];
+//socket server--------------------
+let users = [];
 
-// const addUser = (userId, socketId) => {
-//   !users.some((user) => user.userId === userId) &&
-//     users.push({ userId, socketId });
-// };
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
 
-// const removeUser = (socketId) => {
-//   users = users.filter((user) => user.socketId !== socketId);
-// };
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
 
-// const getUser = (userId) => {
-//   return users.find((user) => user.userId === userId);
-// };
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
 
-// io.on("connection", (socket) => {
-//   // console.log("a user connected!");
+io.on("connection", (socket) => {
+  // console.log("a user connected!");
 
-//   socket.on("addUser", (userId) => {
-//     addUser(userId, socket.id);
-//     io.emit("getUsers", users);
-//   });
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
 
-//   socket.on("disconnect", () => {
-//     // console.log("user disconnected");
-//     removeUser(socket.id);
-//     io.emit("getUsers", users);
-//   });
+  socket.on("disconnect", () => {
+    // console.log("user disconnected");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
 
-//   socket.on("sendMessage", ({ senderId, receiverId, text, time }) => {
-//     const user = getUser(receiverId);
-//     if (user && user.socketId) {
-//       io.to(user.socketId).emit("getMessage", {
-//         senderId,
-//         text,
-//         time,
-//       });
-//     } else {
-//       // console.log("Invalid user or socketId not found.");
-//     }
-//   });
+  socket.on("sendMessage", ({ senderId, receiverId, text, time }) => {
+    const user = getUser(receiverId);
+    if (user && user.socketId) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+        time,
+      });
+    } else {
+      // console.log("Invalid user or socketId not found.");
+    }
+  });
 
-//   socket.on("sendNotification", ({ receiverid, title, msg}) => {
-//     const user = getUser(receiverid);
+  socket.on("sendNotification", ({ receiverid, title, msg}) => {
+    const user = getUser(receiverid);
 
-//     if (user && user.socketId) {
-//       io.to(user.socketId).emit("getNotification", {
-//         title,
-//         msg,
-//       });
-//     } else {
-//       // console.log("Invalid user or socketId not found.");
-//     }
-//   });
-// });
+    if (user && user.socketId) {
+      io.to(user.socketId).emit("getNotification", {
+        title,
+        msg,
+      });
+    } else {
+      // console.log("Invalid user or socketId not found.");
+    }
+  });
+});
