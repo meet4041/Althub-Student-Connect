6bd5760e186c9d2b6b593ce758f608e9b057dbdf
Meet Gandhi <meetgandhi4041@gmail.com>
2025-11-09 11:57:55 +0530

    7th phase

--- Althub-admin/src/baseURL.jsx
+++ Althub-admin/src/baseURL.jsx
@@ -1,1 +1,2 @@
-export const ALTHUB_API_URL = "http://localhost:5001";
+// export const ALTHUB_API_URL = "http://localhost:5001";
+export const WEB_URL = "server-3xvx637ls-meet-gandhis-projects-ef6e26e6.vercel.app:5001";
--- Althub-main/src/baseURL.jsx
+++ Althub-main/src/baseURL.jsx
@@ -1,3 +1,3 @@
 // export const WEB_URL="http://20.106.152.112:5000";
-export const WEB_URL = "http://localhost:5001";
-// export const WEB_URL = "server-delta-one-14.vercel.app:5001";
+// export const WEB_URL = "http://localhost:5001";
+export const WEB_URL = "server-3xvx637ls-meet-gandhis-projects-ef6e26e6.vercel.app:5001";
