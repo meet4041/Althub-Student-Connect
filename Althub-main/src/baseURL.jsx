25b41cd149264ef95686ef6ef08c81b296f54cff
Meet Gandhi <meetgandhi4041@gmail.com>
2025-12-01 13:09:34 +0530

    deployment

--- Althub-main/src/baseURL.jsx
+++ Althub-main/src/baseURL.jsx
@@ -1,5 +1,3 @@
-// If the app is running in "development" mode (npm start), use localhost.
-// Otherwise (npm run build), use the Render URL.
-export const WEB_URL = process.env.NODE_ENV === "development"
+export const ALTHUB_API_URL = process.env.NODE_ENV === "development"
   ? "http://localhost:5001"
-  : "https://althub-student-connect.onrender.com"; // Replace with your actual Render Backend URL
+  : "https://althub-student-connect.onrender.com";
