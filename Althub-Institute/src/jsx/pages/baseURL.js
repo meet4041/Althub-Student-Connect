31226486ed81521cd7000a8bdf4f05520d265fbf
Meet Gandhi <meetgandhi4041@gmail.com>
2025-12-20 20:36:12 +0530

    ok

--- Althub-Institute/src/jsx/pages/baseURL.js
+++ Althub-Institute/src/jsx/pages/baseURL.js
@@ -1,1 +1,7 @@
-export const ALTHUB_API_URL = process.env.REACT_APP_API_URL || "http://localhost:5001";
+// Althub-Institute/src/jsx/pages/baseURL.js
+const RENDER_URL = "https://althub-server.onrender.com"; 
+
+export const ALTHUB_API_URL = 
+  process.env.NODE_ENV === "production" 
+    ? RENDER_URL 
+    : "http://localhost:5001";
