d862a5cd1879f6f16e64db7dbcd5bc9b92d86ba9
jash <61149116+jash011@users.noreply.github.com>
2026-01-02 22:35:29 +0530

    main updated

--- Althub-main/src/baseURL.jsx
+++ Althub-main/src/baseURL.jsx
@@ -1,2 +1,1 @@
-// Looks for REACT_APP_API_URL env var, defaults to localhost if not found
-export const WEB_URL = process.env.REACT_APP_API_URL || "http://localhost:5001";
+export const WEB_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
